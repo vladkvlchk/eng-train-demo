8d8c3a034ab508e71e4898d6b2541dba0ae171f6
Vladyslav <prostoandrei9@gmail.com>
2023-05-29 20:05:47 +0300

    mobile adaptive

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App: React.FC = () => {
           element={
             <>
               <Home />
-              <Account />
+              {/* <Account /> */}
             </>
           }
         />
